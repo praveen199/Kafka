1bb9928ff6c237fee6a4659b4fc363b9daf75e61
Praveen Singh <praveensingh.199@gmail.com>
2026-04-11 11:15:07 +0530

    Kafka retry topic

---
+++ kafka-retry-topic/src/main/java/com/spring/kafka/kafka_retry_topic/KafkaRetryTopicApplication.java
@@ -0,0 +1,13 @@
+package com.spring.kafka.kafka_retry_topic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaRetryTopicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaRetryTopicApplication.class, args);
+	}
+
+}
---
+++ kafka-retry-topic/src/test/java/com/spring/kafka/kafka_retry_topic/KafkaRetryTopicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.kafka.kafka_retry_topic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaRetryTopicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
